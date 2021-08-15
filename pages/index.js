0bcd6939320edb5b4a12cdc4f8c36c0d15106e4f
Zack Schwartz <ztschwartz@icloud.com>
2021-08-14 21:42:36 -0500

    text changes

--- pages/index.js
+++ pages/index.js
@@ -89,19 +89,8 @@ export default function Home({loaded , loaderView}) {
                 <div className="col-5--7 contentGroup">
                   <div className="textGroup">
                     <p className="softText textBody1Medium">Hey again, I’m Zack Schwartz. Recently I’ve been particularly passionate about the design process and the communication between designers and developers.</p>
-                  </div>
-                  <div className="textGroup">
-                    {/* <p className="mainText textTitleMedium">Start with a well defined problem.</p> */}
-                    <p className="mainText textTitleMedium">Hey again, I’m Zack Schwartz. I’m a designer, developer, and problem solver currently living in St. Louis, Missouri. Recently I’ve been particularly passionate about the design process and the communication between designers and developers.</p>
-                  </div>
-                  <div className="textGroup">
-                    <p className="mainText textTitleMedium">Start with a well defined problem.</p>
-                    <p className="softText textBody1Medium">Hey again, I’m Zack Schwartz. I’m a designer, developer, and problem solver currently living in St. Louis, Missouri. Recently I’ve been particularly passionate about the design process and the communication between designers and developers.</p>
-                  </div>
-                  <div className="textGroup">
-                    <p className="mainText textTitleMedium">Start with a well defined problem.</p>
-                    <p className="softText textBody1Medium">Hey again, I’m Zack Schwartz. I’m a designer, developer, and problem solver currently living in St. Louis, Missouri. Recently I’ve been particularly passionate about the design process and the communication between designers and developers.</p>
-                    <p className="softText textBody1Medium">Hey again, I’m Zack Schwartz. I’m a designer, developer, and problem solver currently living in St. Louis, Missouri. Recently I’ve been particularly passionate about the design process and the communication between designers and developers.</p>
+                    <p className="softText textBody1Medium">Hey again, I’m Zack Schwartz. Recently I’ve been particularly passionate about the design process and the communication between designers and developers.</p>
+                    <p className="softText textBody1Medium">Hey again, I’m Zack Schwartz. Recently I’ve been particularly passionate about the design process and the communication between designers and developers.</p>
                   </div>
                 </div>
               </div>

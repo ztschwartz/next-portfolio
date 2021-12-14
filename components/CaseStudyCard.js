d3ed3778e4966feee89571bb1400cb08e79e1e62
Zack Schwartz <ztschwartz@icloud.com>
2021-12-14 15:15:26 -0600

    changes

--- components/CaseStudyCard.js
+++ components/CaseStudyCard.js
@@ -22,6 +22,7 @@ const CaseStudyCard = ({title, subtitle, slug, img , postData}) => {
                             <svg className="fillAccent" viewBox="0 0 41 41" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M32.9565 4.85372C33.051 4.75923 32.9841 4.59766 32.8505 4.59766H0.0410156V2.09766C0.0410156 1.26923 0.71259 0.597656 1.54102 0.597656H40.041V39.0977C40.041 39.9261 39.3694 40.5977 38.541 40.5977H36.041V7.78822C36.041 7.65458 35.8794 7.58766 35.785 7.68215L3.45523 40.0119L0.626802 37.1834L32.9565 4.85372Z"/>
                             </svg>
+                            
                          </div>
                     </div>
                 </div>

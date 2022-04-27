989c88f47752a6986c8f916edc4e7370f5376da3
Zack Schwartz <ztschwartz@icloud.com>
2022-04-26 22:57:23 -0500

    changed environment variables to correct name

--- utils/contentful-client.js
+++ utils/contentful-client.js
@@ -14,6 +14,6 @@ export const fetchContentfulData = async () => {
 };
 
 export const client = contentful.createClient({
-	space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-	accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+	space: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });

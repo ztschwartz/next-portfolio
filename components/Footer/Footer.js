90d3b10d4576bb116baf2a5e4fa33eace9292e8b
Zack Schwartz <ztschwartz@icloud.com>
2022-11-03 12:15:23 -0500

    updated footer main section

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,43 +7,50 @@ import { GridContainer, GridChild } from "../Grid/Grid.js";
 const Footer = () => {
 	return (
 		<footer className={`${style.footer} bgDeep`}>
-			<GridContainer className={style.contentSection}>
-				<GridChild colSpan={4} className={`${style.navigationSection}`}>
-					<h3 className="textMain textP2 textBold">Navigation</h3>
-					<div className={style.linksListSection}>
-						<a className="textP2 link textSoft textMedium">Introduction</a>
-						<a className="textP2 link textSoft textMedium">Work</a>
-						<a className="textP2 link textSoft textMedium">About</a>
-						<a className="textP2 link textSoft textMedium">Contact</a>
-					</div>
-				</GridChild>
-				<GridChild colSpan={4} className={`${style.caseStudiesSection}`}>
-					<h3 className="textMain textP2 textBold">Case Studies</h3>
-					<div className={style.linksListSection}>
-						<p className="textP2 textSoft textMedium">Coming Soon</p>
-					</div>
-				</GridChild>
-				<GridChild colSpan={4} className={`${style.contactSection}`}>
-					<h3 className="textMain textP2 textBold">Contact</h3>
-					<div className={style.linksListSection}>
-						<a className="textP2 link textSoft textMedium">
-							hello@zacktschwartz
-						</a>
-						<a className="textP2 link textSoft textMedium">
-							Message me on LinkedIn
-						</a>
-						<a className="textP2 link textSoft textMedium">
-							Message me on Polywork
-						</a>
-					</div>
-				</GridChild>
-				<GridChild colSpan={12} className={`${style.infoSection}`}>
-					<LogoIcon />
-					<p className="textP2 textSoft textMedium">
-						©2022 Zachary Schwartz. All rights reserved.
-					</p>
-				</GridChild>
-			</GridContainer>
+			<div className={style.contentSection}>
+				<GridContainer className={style.footerMain}>
+					<GridChild colSpan={4} className={`${style.navigationSection}`}>
+						<h3 className="textMain textP2 textBold">Navigation</h3>
+						<div className={style.linksListSection}>
+							<a className="textP2 link textSoft textMedium">Introduction</a>
+							<a className="textP2 link textSoft textMedium">Work</a>
+							<a className="textP2 link textSoft textMedium">About</a>
+							<a className="textP2 link textSoft textMedium">Contact</a>
+						</div>
+					</GridChild>
+					<GridChild colSpan={4} className={`${style.caseStudiesSection}`}>
+						<h3 className="textMain textP2 textBold">Case Studies</h3>
+						<div className={style.linksListSection}>
+							<p className="textP2 textSoft textMedium">Coming Soon</p>
+						</div>
+					</GridChild>
+					<GridChild colSpan={4} className={`${style.contactSection}`}>
+						<h3 className="textMain textP2 textBold">Contact</h3>
+						<div className={style.linksListSection}>
+							<a className="textP2 link textSoft textMedium">
+								hello@zacktschwartz
+							</a>
+							<a className="textP2 link textSoft textMedium">
+								Message me on LinkedIn
+							</a>
+							<a className="textP2 link textSoft textMedium">
+								Message me on Polywork
+							</a>
+						</div>
+					</GridChild>
+				</GridContainer>
+				<GridContainer>
+					<GridChild colSpan={12} className={`${style.infoSection}`}>
+						<LogoIcon />
+						<p className="textP2  textSoft textMedium">
+							<span className={style.infoSectionText}>
+								©2022&nbsp;Zachary&nbsp;Schwartz.
+							</span>{" "}
+							<span>All&nbsp;rights&nbsp;reserved.</span>
+						</p>
+					</GridChild>
+				</GridContainer>
+			</div>
 		</footer>
 	);
 };
